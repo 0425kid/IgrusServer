4c113cc5a779736fe83b3b2bd7511a96b18f1558
0425kid <76833239+0425kid@users.noreply.github.com>
2022-11-02 16:58:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 app.get('/', (req, res) => res.sendFile(__dirname + '/igrus.html'));
 
 // app.listen(3000, () => console.log('Example app listening on port 3000!'));
-app.listen(80, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
